facbd5881da2711ea6522511899235963f346fdb
Yaroslav <yafyber@gmail.com>
2019-10-06 19:37:41 +0300

    ended day2

---
+++ homework-2/hw2ex01.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func inputInt(msg string) (value string) {
+	fmt.Println(msg)
+	fmt.Scan(&value)
+	return
+}
+
+func main() {
+
+	var g int64
+	var err error
+
+	for {
+		g, err = strconv.ParseInt(inputInt("Enter integer number"), 10, 32)
+		if err == nil{
+			break
+		}
+	}
+	if g % 2 == 0 {
+	fmt.Println("The even")
+	} else {
+	fmt.Println("The odd")
+	}
+}
