77ff58816234456020d836feb230c93cd3070318
Yaroslav <yafyber@gmail.com>
2019-10-18 00:15:42 +0300

    finally

--- homework-4/hw4ex04.go
+++ homework-4/hw4ex04.go
@@ -27,75 +27,29 @@ type canGo []knightPosition
 
 var allPoint canGo
 
-func (pos knightPosition) FirstPoint() {
-	pos.x += 1
-	pos.y += 2
-	if pos.x < 8 && pos.y < 8 {
-		allPoint = append(allPoint, pos)
-	}
-}
-func (pos knightPosition) SecondPoint() {
-	pos.x += 1
-	pos.y -= 2
-	if pos.x < 8 && pos.y > 0 {
-		allPoint = append(allPoint, pos)
-	}
-}
-func (pos knightPosition) FirdPoint() {
-	pos.x -= 1
-	pos.y += 2
-	if pos.x > 0 && pos.y < 8 {
-		allPoint = append(allPoint, pos)
-	}
-}
-func (pos knightPosition) FourthPoint() {
-	pos.x -= 1
-	pos.y -= 2
-	if pos.x > 0 && pos.y > 0 {
-		allPoint = append(allPoint, pos)
-	}
-}
-func (pos knightPosition) FifthPoint() {
-	pos.x += 2
-	pos.y += 1
-	if pos.x < 8 && pos.y < 8 {
-		allPoint = append(allPoint, pos)
-	}
-}
-func (pos knightPosition) SixthPoint() {
-	pos.x -= 2
-	pos.y += 1
-	if pos.x > 0 && pos.y < 8 {
-		allPoint = append(allPoint, pos)
-	}
-}
-func (pos knightPosition) SeventhPoint() {
-	pos.x += 2
-	pos.y -= 1
-	if pos.x < 8 && pos.y > 0 {
-		allPoint = append(allPoint, pos)
-	}
-}
-func (pos knightPosition) EigthPoint() {
-	pos.x -= 2
-	pos.y -= 1
-	if pos.x > 0 && pos.y > 0 {
+func (pos knightPosition) OtherPoint(x, y int) {
+	pos.x += x
+	pos.y += y
+	if pos.x > 0 && pos.x < 8 && pos.y > 0 && pos.y < 8 {
 		allPoint = append(allPoint, pos)
 	}
 }
 
 func (pos *knightPosition) FindWay() (canGo, error) {
+	movePoint := canGo{
+		{1, 2},
+		{1, -2},
+		{-1, 2,},
+		{-1, -2,},
+	}
+
 	if pos.x < 1 || pos.y < 1 || pos.x > 8 || pos.y > 8 {
 		return nil, wrong()
 	}
-	pos.FirstPoint()
-	pos.SecondPoint()
-	pos.FirdPoint()
-	pos.FourthPoint()
-	pos.FifthPoint()
-	pos.SixthPoint()
-	pos.SeventhPoint()
-	pos.EigthPoint()
+	for _, val := range movePoint {
+		pos.OtherPoint(val.y, val.x)
+		pos.OtherPoint(val.x, val.y)
+	}
 	return allPoint, nil
 }
 
@@ -104,7 +58,7 @@ func (pos knightPosition) String() string {
 }
 
 func main() {
-	some := knightPosition{5, 5}
+	some := knightPosition{1, 1}
 	arr, err := some.FindWay()
 	if err == nil {
 		fmt.Println(arr)
